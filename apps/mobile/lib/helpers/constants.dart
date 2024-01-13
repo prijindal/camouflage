a5bedb42d7a256cfddba6fb7e1346a1b127e7cb1
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2024-01-13 22:23:06 +0530

    Added production url

--- apps/mobile/lib/helpers/constants.dart
+++ apps/mobile/lib/helpers/constants.dart
@@ -2,4 +2,5 @@ const ITERATIONS = 10000;
 const HASH_LENGTH = 256;
 
 const appThemeMode = "APP_THEME_MODE";
-const baseUrl = "http://192.168.1.2:3000";
+// const baseUrl = "http://192.168.1.2:3000";
+const baseUrl = "https://camouflage.43.204.229.12.nip.io";
