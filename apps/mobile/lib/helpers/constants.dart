6a537fbdc8d4cf8c7af25d56a0b63259cd648f59
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2024-01-14 22:41:01 +0530

    fixed default base url

--- apps/mobile/lib/helpers/constants.dart
+++ apps/mobile/lib/helpers/constants.dart
@@ -5,6 +5,6 @@ const appThemeMode = "APP_THEME_MODE";
 
 String baseUrl = const String.fromEnvironment(
   "BASE_URL",
-  // defaultValue: "https://camouflage.43.204.229.12.nip.io",
-  defaultValue: "http://192.168.1.2:3000",
+  defaultValue: "https://camouflage.43.204.229.12.nip.io",
+  // defaultValue: "http://192.168.1.2:3000",
 );
