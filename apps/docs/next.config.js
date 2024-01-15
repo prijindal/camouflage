c882ae75781855870e0f1bf7504423d6b0c7e49a
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2024-01-15 13:20:13 +0530

    moved mobile folder to flutter

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -5,6 +5,7 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
   output: "standalone",
+  basePath: process.env.BASE_PATH || "/docs",
   experimental: {
     outputFileTracingRoot: path.join(__dirname, "../../"),
   },
